0161dfaadb36e9f9bd414f7f3ecca9d2921c77f3
simone pacileo <simone.pacileo@piksel.com>
2022-11-18 14:39:20 +0100

    add getUsers
    Add the method that return a json of users

--- src/main/java/com/tesi/kcspringboot/controller/UserController.java
+++ src/main/java/com/tesi/kcspringboot/controller/UserController.java
@@ -1,10 +1,15 @@
 package com.tesi.kcspringboot.controller;
 
+import com.tesi.kcspringboot.model.UserDTO;
+import org.keycloak.representations.idm.UserRepresentation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/user")
@@ -16,7 +21,24 @@ public class UserController {
         return new ResponseEntity<>("Hi!", HttpStatus.OK);
     }
 
-}
+@RequestMapping(value = "/getUsers", method = RequestMethod.GET)
+    public ResponseEntity<?> getUsers(){
 
+        //UserRepresentation userRepresentation = new UserRepresentation();
+        List<UserDTO> listUser = new ArrayList<>();
+    UserDTO user1 = new UserDTO("Simone", "Pacileo", "simone.pacileo@piksel.com" , "", "" );
+    UserDTO user2 = new UserDTO("Noemi", "Santamaria", "noemi.santamaria@piksel.com" , "", "");
+    UserDTO user3 = new UserDTO("Marco", "Amenta", "marco.amenta@piksel.com", "", "");
+        listUser.add(user1);
+        listUser.add(user2);
+        listUser.add(user3);
+
+
+
+        return new ResponseEntity<>(listUser,HttpStatus.OK);
+
+    }
+
+}
 
 
--- src/main/java/com/tesi/kcspringboot/model/UserDTO.java
+++ src/main/java/com/tesi/kcspringboot/model/UserDTO.java
@@ -1,27 +1,19 @@
 package com.tesi.kcspringboot.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+
+import lombok.*;
 
 import java.io.Serializable;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Data
 public class UserDTO implements Serializable {
-
-    private static final long serialVersionUID = 1L;
-
-    private String userName;
-
-    private String emailAddress;
-
     private String firstName;
 
     private String lastName;
-
+    private String email;
+    private String userName;
     private String password;
-
-
 }
