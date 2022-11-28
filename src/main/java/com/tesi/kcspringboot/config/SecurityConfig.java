4187e826696abbab61a9efe954bb1bcf367f97f9
simone pacileo <simone.pacileo@piksel.com>
2022-11-28 17:18:08 +0100

    Minor fix to access method getUsers)
    add annotation preauthorize has role

--- src/main/java/com/tesi/kcspringboot/config/SecurityConfig.java
+++ src/main/java/com/tesi/kcspringboot/config/SecurityConfig.java
@@ -1,48 +1,34 @@
 package com.tesi.kcspringboot.config;
 
-import org.keycloak.adapters.springsecurity.KeycloakSecurityComponents;
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
 import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
-@Configuration
-@EnableWebSecurity
-@ComponentScan(basePackageClasses = KeycloakSecurityComponents.class)
-public class SecurityConfig   extends  KeycloakWebSecurityConfigurerAdapter{
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-
-        super.configure(http);
-        //     http.antMatcher("/user").authorizeRequests().anyRequest().hasAnyRole("administrator");
-//        http.authorizeRequests().antMatchers("/user*")
-////                .hasAuthority("administrator").anyRequest().authenticated();
-        http.authorizeRequests().antMatchers("/user/").hasRole("[administrator]").anyRequest().authenticated();
-        http.csrf().disable();
-    }
-
-
-
-
-
+@KeycloakConfiguration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@Import({KeycloakSpringBootConfigResolver.class})
+public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
 
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-//
+
         KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
-       keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
+        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
         auth.authenticationProvider(keycloakAuthenticationProvider);
     }
+
 
     @Bean
     @Override
@@ -50,21 +36,27 @@ public class SecurityConfig   extends  KeycloakWebSecurityConfigurerAdapter{
         return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        super.configure(http);
 
 
-//    public KeycloakSecurityContext getKeycloakSecurityContext(){
-//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-//        KeycloakAuthenticationToken token;
-//        KeycloakSecurityContext context;
-//        if (authentication == null){
-//            throw new IllegalStateException("Cannot set authorization  beacuse there is not authenticated principal");
-//        }
-//        if (! KeycloakAuthenticationToken.class.isAssignableFrom(authentication.getClass())){
-//            throw new IllegalStateException(String.format("Cannot set authorization header because Authentication is of type %s but %s is required",
-//                    authentication.getClass(), KeycloakAuthenticationToken.class));
-//        }
-//        token= (KeycloakAuthenticationToken) authentication;
-//        context = token.getAccount().getKeycloakSecurityContext();
-//        return context;
+        http.authorizeRequests().antMatchers("/user/*").hasRole("administrator").anyRequest().permitAll();
+
+        http.csrf().disable();
+    }
+
+//    @Bean
+//    public Keycloak keycloak(KeycloakSpringBootProperties props) {
+//        return KeycloakBuilder.builder()
+//                .serverUrl(props.getAuthServerUrl())
+//                .realm(props.getRealm()) //
+//                .grantType(OAuth2Constants.CLIENT_CREDENTIALS)
+//                .clientId(props.getResource())
+//                .clientSecret((String) props.getCredentials().get("secret"))
+//                .resteasyClient(new ResteasyClientBuilder().connectionPoolSize(10).build()).build();
 //    }
+
+
 }
--- src/main/java/com/tesi/kcspringboot/controller/UserController.java
+++ src/main/java/com/tesi/kcspringboot/controller/UserController.java
@@ -1,12 +1,10 @@
 package com.tesi.kcspringboot.controller;
 
-import com.tesi.kcspringboot.model.UserDTO;
-import org.apache.commons.codec.binary.StringUtils;
 import org.keycloak.KeycloakPrincipal;
 import org.keycloak.KeycloakSecurityContext;
 import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
 import org.keycloak.representations.AccessToken;
-import org.keycloak.representations.account.UserRepresentation;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -15,26 +13,22 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
 
 @RestController
 @RequestMapping(value = "/user")
 public class UserController {
-//
-//    @Value("${keycloak.auth-server-url}")
-//    private String serverUrl;
-//
-//    @Value("${keycloak.resource}")
-//    private String resource;
-//
-//    @Value("${keycloak.credentials.secret}")
-//    private String credentials_secret;
-//
-//    @Value(("${keycloak.realm}"))
-//    private String realm;
+    //
+    @Value("${keycloak.auth-server-url}")
+    private String serverUrl;
+    //
+    @Value("${keycloak.resource}")
+    private String resource;
+    //
+    @Value("${keycloak.credentials.secret}")
+    private String credentials_secret;
+    //
+    @Value(("${keycloak.realm}"))
+    private String realm;
 
     //TODO da rimuovere
 
@@ -43,9 +37,9 @@ public class UserController {
         return new ResponseEntity<>("Hi!", HttpStatus.OK);
     }
 
-
-
+    @PreAuthorize("hasRole('administrator')")
     @RequestMapping(value = "/getUsers", method = RequestMethod.GET)
+
     // this method can be accessed by user whose  role is admin
     public ResponseEntity<?> getUsers(HttpServletRequest request) {
 
@@ -55,34 +49,38 @@ public class UserController {
         AccessToken accessToken = session.getToken();
         String a = principal.getName();
         String username = accessToken.getPreferredUsername();
-        String emalId = accessToken.getEmail();
+        String email = accessToken.getEmail();
         String lastName = accessToken.getFamilyName();
         String firstName = accessToken.getGivenName();
         String realmNane = accessToken.getIssuer();
         AccessToken.Access realmAccess = accessToken.getRealmAccess();
         String role = realmAccess.getRoles().toString();
-        // UserRepresentation userRepresentation = new  UserRepresentation();
-        List<UserDTO> listUser = new ArrayList<>();
-        String match = "[administrator]";
+
         //to manage the string also null
-       if ((StringUtils.equals(role, match))) {
+        // if ((StringUtils.equals(role, match))) {
+        //    Keycloak keycloak = Keycloak.getInstance(serverUrl,realmNane, username, resource, credentials_secret );
+        //   List<UserRepresentation> listUsers = keycloak.realm(realmNane).users().list();
+        //    RealmRepresentation realmRepr = (RealmRepresentation) keycloak.realm(realmNane).toRepresentation().getUsers();
+        //   List<UserRepresentation> realmRepresentation =  keycloak.realm(realm).toRepresentation().getUsers();
+        // list.add(response)
+//
+        //  listUsers.add((UserRepresentation) keycloak.realm(realm).toRepresentation().getUsers());
+
+//            UserDTO user1 = new UserDTO("Simone", "Pacileo", "simone.pacileo@piksel.com", "", "");
+//            UserDTO user2 = new UserDTO("Noemi", "Santamaria", "noemi.santamaria@piksel.com", "", "");
+//            UserDTO user3 = new UserDTO("Marco", "Amenta", "marco.amenta@piksel.com", "", "");
+//            listUser.add(user1);
+//            listUser.add(user2);
+//            listUser.add(user3);
+
+        //       return new ResponseEntity<>("Users", HttpStatus.OK);
 
 
-            UserDTO user1 = new UserDTO("Simone", "Pacileo", "simone.pacileo@piksel.com", "", "");
-            UserDTO user2 = new UserDTO("Noemi", "Santamaria", "noemi.santamaria@piksel.com", "", "");
-            UserDTO user3 = new UserDTO("Marco", "Amenta", "marco.amenta@piksel.com", "", "");
-            listUser.add(user1);
-            listUser.add(user2);
-            listUser.add(user3);
-
-            return new ResponseEntity<>(listUser, HttpStatus.OK);
-       }
-
-       // System.out.println(role);
         return new ResponseEntity<>("Ciao utente: \n " +
                 " username: " + username + "\n" +
-                " email:  " + emalId + "\n" +
-                " role:  " +  role + "\n", HttpStatus.OK);
+                " email:  " + email + "\n" +
+                " role:  " + role + "\n", HttpStatus.OK);
+
 
     }
 
