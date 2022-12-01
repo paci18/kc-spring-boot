669bfa496e043971c5d4d7de866715f778bed1e9
simone pacileo <simone.pacileo@piksel.com>
2022-12-01 10:15:55 +0100

    convert json UserRepresentation to list UserDto
    add method that convert json of userRepresentation to list of UserDTO

--- src/main/java/com/tesi/kcspringboot/controller/UserController.java
+++ src/main/java/com/tesi/kcspringboot/controller/UserController.java
@@ -1,43 +1,32 @@
 package com.tesi.kcspringboot.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.tesi.kcspringboot.DTO.UserDTO;
 import com.tesi.kcspringboot.service.KcService;
 import org.keycloak.KeycloakPrincipal;
 import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
-import org.keycloak.representations.AccessToken;
-import org.keycloak.representations.idm.RealmRepresentation;
 import org.keycloak.representations.idm.UserRepresentation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.GET;
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/user")
 public class UserController {
-
-     private  final KcService kcService;
-
+    @Autowired
+    private final KcService kcService;
     public UserController(KcService kcService) {
         this.kcService = kcService;
+
     }
-    //
-//    @Value("${keycloak.auth-server-url}")
-//    private String serverUrl;
-//    //
-//    @Value("${keycloak.resource}")
-//    private String resource;
-//    //
-//    @Value("${keycloak.credentials.secret}")
-//    private String credentials_secret;
-//    //
-//    @Value(("${keycloak.realm}"))
-//    private String realm;
 
     //TODO da rimuovere
 
@@ -47,34 +36,22 @@ public class UserController {
     }
 
     @PreAuthorize("hasRole('administrator')")
-    @RequestMapping(value = "/getUsers", method = RequestMethod.GET)
-
-
-    // this method can be accessed by user whose  role is admin
+    @GetMapping(value = "/getUsers")
     public ResponseEntity<?> getUsers(HttpServletRequest request) {
 
 
         KeycloakAuthenticationToken AuthenticationToken = (KeycloakAuthenticationToken) request.getUserPrincipal();
         KeycloakPrincipal principal = (KeycloakPrincipal) AuthenticationToken.getPrincipal();
-        AccessToken Accesstoken = principal.getKeycloakSecurityContext().getToken();
+      //  AccessToken Accesstoken = principal.getKeycloakSecurityContext().getToken();
         String realmRepresentation = principal.getKeycloakSecurityContext().getRealm();
 
         List<UserRepresentation> listUsers = kcService.getUsersFromToken(realmRepresentation);
 
-        //System.out.println("ciao");
-//            UserDTO user1 = new UserDTO("Simone", "Pacileo", "simone.pacileo@piksel.com", "", "");
-//            UserDTO user2 = new UserDTO("Noemi", "Santamaria", "noemi.santamaria@piksel.com", "", "");
-//            UserDTO user3 = new UserDTO("Marco", "Amenta", "marco.amenta@piksel.com", "", "");
-//            listUser.add(user1);
-//            listUser.add(user2);
-//            listUser.add(user3);
 
-        //       return new ResponseEntity<>("Users", HttpStatus.OK);
+        //  return new ResponseEntity<>(kcService.listaStringheUsers(listUsers), HttpStatus.OK);
+         //return new ResponseEntity<>(listUsers, HttpStatus.OK);
 
-
-        //return new ResponseEntity<>("Ciao utente admin" ,HttpStatus.OK);
-     //   return  new ResponseEntity<>( listUser, HttpStatus.OK);
-      return new ResponseEntity<>(listUsers, HttpStatus.OK);
+        return new ResponseEntity<>(kcService.ConvertUserReprToUSerDTO(listUsers), HttpStatus.OK);
 
 
     }
--- src/main/java/com/tesi/kcspringboot/service/KcService.java
+++ src/main/java/com/tesi/kcspringboot/service/KcService.java
@@ -1,9 +1,13 @@
 package com.tesi.kcspringboot.service;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.tesi.kcspringboot.DTO.UserDTO;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.representations.idm.UserRepresentation;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -11,41 +15,56 @@ import java.util.List;
 public class KcService {
 
 
-         private final  Keycloak keycloakService ;
+    private final Keycloak keycloakService;
 
     public KcService(Keycloak keycloakService) {
         this.keycloakService = keycloakService;
     }
 
-//    public  List<String> getUsersFromToken( String realm) {
+    //    public  List<String> getUsersFromToken( String realm) {
 //        List<String> listOfUsers = new ArrayList<>();
 //        listOfUsers.add("Simone Pacileo");
 //        listOfUsers.add("Marco Amenta");
 //        listOfUsers.add("Noemi Santamaria");
-   public  List<UserRepresentation> getUsersFromToken( String realm) {
-      List<UserRepresentation> listOfUsersWithManyInformation= keycloakService.realm(realm).users().list();
-//      List<String> listOfUsers = new ArrayList<>();
+    public List<UserRepresentation> getUsersFromToken(String realm) {
+        List<UserRepresentation> listOfUsersWithManyInformation = keycloakService.realm(realm).users().list();
+
+
 //
-//      for (int i = 0; i<listOfUsersWithManyInformation.size(); i++){
-//            listOfUsers.add(listOfUsersWithManyInformation.get(i).getUsername());
-//          listOfUsers.add(listOfUsersWithManyInformation.get(i).getFirstName());
-//          listOfUsers.add(listOfUsersWithManyInformation.get(i).getLastName());
-//          listOfUsers.add(listOfUsersWithManyInformation.get(i).getEmail());
-//      }
-//
-//       List<UserRepresentation> prova = new ArrayList<>();
-//       for (int i = 0; i<listOfUsers.size(); i++){
-//           prova.subList(i, prova.size());
+////
+//        for (int i =0; i<listOfUsers.size(); i++){
+//            System.out.println(listOfUsers.get(i));
+//        }
 //
 //
-       return  listOfUsersWithManyInformation;
-}
+        return listOfUsersWithManyInformation;
+    }
 
 //            for (int i=0; i<listUsers.size(); i++) {
 //                System.out.println(listUsers.get(i));
 //            }
 //
-            //return listOfUsersWithManyInformation;
-        //}
+    //return listOfUsersWithManyInformation;
+    //}
 
+
+    public List<String> listaStringheUsers(List<UserRepresentation> listUsers) {
+        List<String> listOfUsers = new ArrayList<>();
+        for (UserRepresentation listUser : listUsers) {
+            listOfUsers.add(listUser.getUsername());
+            listOfUsers.add(listUser.getFirstName());
+            listOfUsers.add(listUser.getLastName());
+            listOfUsers.add(listUser.getEmail());
+        }
+
+        return listOfUsers;
+    }
+
+
+    public List<UserDTO> ConvertUserReprToUSerDTO(List<UserRepresentation> userRepresentation) {
+        TypeReference<List<UserDTO>> typeReference = new TypeReference<>() {
+        };
+        return JsonMapper.builder().build().convertValue(userRepresentation, typeReference);
+
+    }
 }
